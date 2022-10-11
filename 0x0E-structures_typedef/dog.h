6c2ed82592bb76e15ca56413c7e01a2e39bbf27c
samwelopondo <opondosamwel2@gmail.com>
2022-10-11 13:36:01 +0300

    Structures and typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct my_struct - Short description
+ * @*name: First member
+ * @age: Second member
+ * @*owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
