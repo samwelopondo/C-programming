cad10b9bd25577652fa166c11f3b0405fd81b4ef
samwelopondo <opondosamwel2@gmail.com>
2022-10-13 17:04:33 +0300

    Function Pointers

--- 0x0F-function_pointers/main.c
+++ 0x0F-function_pointers/main.c
@@ -2,50 +2,42 @@
 #include "function_pointers.h"
 #include "_putchar.c"
 /**
- *print_name - prints a name
- *@name: name to print
- *@f: function pointer passed as argument
+ *array_iterator - executes a function given as a parameter on
+ *each element of an array.
+ *
+ *
  */
-void print_name(char *name, void (*f)(char *))
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	f(name);
-}
+	if (!array || !size || !action)
+		return;
+	int i;
 
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}	
+}
 /**
- * print_name_as_is - prints a name as is
- * @name: name of the person
+ * print_elem - prints an integer
+ * @elem: the integer to print
  *
  * Return: Nothing.
  */
-void print_name_as_is(char *name)
+void print_elem(int elem)
 {
-    printf("Hello, my name is %s\n", name);
+    printf("%d\n", elem);
 }
 
 /**
- * print_name_uppercase - print a name in uppercase
- * @name: name of the person
+ * print_elem_hex - prints an integer, in hexadecimal
+ * @elem: the integer to print
  *
  * Return: Nothing.
  */
-void print_name_uppercase(char *name)
+void print_elem_hex(int elem)
 {
-    unsigned int i;
-
-    printf("Hello, my uppercase name is ");
-    i = 0;
-    while (name[i])
-    {
-        if (name[i] >= 'a' && name[i] <= 'z')
-        {
-            putchar(name[i] + 'A' - 'a');
-        }
-        else
-        {
-            putchar(name[i]);
-        }
-        i++;
-    }
+    printf("0x%x\n", elem);
 }
 
 /**
@@ -55,9 +47,9 @@ void print_name_uppercase(char *name)
  */
 int main(void)
 {
-    print_name("Bob", print_name_as_is);
-    print_name("Bob Dylan", print_name_uppercase);
-    printf("\n");
+    int array[5] = {0, 98, 402, 1024, 4096};
+
+    array_iterator(array, 5, &print_elem);
+    array_iterator(array, 5, &print_elem_hex);
     return (0);
 }
-
