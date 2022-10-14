969470d81f355a23f292a4e5397a8b74f77f7e75
samwelopondo <opondosamwel2@gmail.com>
2022-10-14 21:21:40 +0300

    Variadic Functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,9 @@
 #ifndef VARIADIC_FUNCTION
 #define VARIADIC_FUNCTION
-
 #include <stdlib.h>
 #include <stdarg.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 
-#endif 
+#endif
