806d8a9576c23071e369b3dc5ab89acc2d30d45d
samwelopondo <opondosamwel2@gmail.com>
2022-11-03 12:42:30 +0300

    File I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,8 +1,16 @@
+/**
+ * read_textfile - that reads a text file and prints
+ * @filename: variable pointer
+ * @letters: size letters
+ * Description: Write a function that reads a text file and prints it
+ * to the POSIX standard output.
+ * Return: the actual number of letters it could read and print, 0 otherwise
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	char *let;
-	size_t red;
+	size_t red, w;
 
 	if (filename == NULL)
 		return (0);
@@ -15,7 +23,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 
-	write(STDIN_FILENO, let, red);
+	w = write(STDIN_FILENO, let, red);
 	if (STDIN_FILENO == -1 || red != letters)
 		return (0);
+	close(fd);
+	return (w);
 }
