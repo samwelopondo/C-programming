fe32f0e6ffd028d556d3390537fb709a1c9e2580
samwelopondo <opondosamwel2@gmail.com>
2022-11-03 14:47:01 +0300

    File I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -36,6 +36,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	w = write(STDOUT_FILENO, text, let);
 
+	if (w == -1 || let != letters)
+		return (0);
 	close(file);
 
 	return (w);
