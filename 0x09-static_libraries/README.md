73e322144b522dffbc76e88217a2fd29ee848d66
samwelopondo <opondosamwel2@gmail.com>
2022-12-06 15:09:39 +0300

    Low level Programming

--- 0x09-static_libraries/README.md
+++ 0x09-static_libraries/README.md
@@ -1,2 +1,2 @@
-<a href = "https://github.com/Gtindi/alx-low_level_programming">Back</a>
+<a href = "https://github.com/samwelopondo/alx-low_level_programming">Back</a>
 <h1> This project is about C - Static libraries </h1>
---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    printf("Sizeof char is %ld bytes\n",sizeof(char));
+    printf("Sizeof int is %ld bytes\n",sizeof(int));
+    return (1);
+}
